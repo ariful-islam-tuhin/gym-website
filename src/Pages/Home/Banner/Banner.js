c8af93fc8996611f155eefa82d89290b80d657fc
Ariful Islam <ariful.islam.tuhin.1830@gmail.com>
2021-12-16 08:41:21 -0800

    first commit

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <img className='' src="https://image.freepik.com/free-photo/woman-doing-squats-smith-machine_7502-9063.jpg" />
+            
+        </div>
+    );
+};
+
+export default Banner;
