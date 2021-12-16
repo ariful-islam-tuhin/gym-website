360c2598551da198ed2fa9f99aab4636dfb91dd7
Ariful Islam <ariful.islam.tuhin.1830@gmail.com>
2021-12-16 09:31:13 -0800

    commit

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -4,6 +4,7 @@ const Banner = () => {
     return (
         <div>
             <img className='' src="https://image.freepik.com/free-photo/woman-doing-squats-smith-machine_7502-9063.jpg" />
+            <h1>this is a good banner</h1>
             
         </div>
     );
